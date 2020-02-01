f318a18d2191e582ee0db887e88970849d3ffd38
S. Makai Takori <makait87@gmail.com>
2020-02-01 02:33:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,13 @@
 class Transfer
   
-  def initialize
+  attr_accessor :sender, :receiver, :transfer_amount
+  
+  def initialize(sender, transfer amount = )
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    
+  
   
   end
   
