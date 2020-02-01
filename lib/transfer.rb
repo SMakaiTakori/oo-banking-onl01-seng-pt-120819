effab25dd667121ac67c71463f8bd485d1edaa97
S. Makai Takori <makait87@gmail.com>
2020-02-01 02:57:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ class Transfer
   end  
   
   def reverse_transfer
-    if @status = "complete"
+    if execute_transaction == true
       sender.balance += amount
       receiver.balance -= amount
       @status = "reversed"
