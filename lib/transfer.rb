976c94803d674e087ed96f8d20cf90ceccaad96c
S. Makai Takori <makait87@gmail.com>
2020-02-01 02:51:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end  
   
   def execute_transaction
-    if valid? && sender.balance  
+    if valid? && sender.balance >= amount 
       sender.balance -= amount
       receiver.balance += amount
       @status = "complete"
